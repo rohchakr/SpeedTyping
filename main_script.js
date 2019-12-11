8981add9cc8daeb48c5865e3827d8e4af7479d97
Rohit Chakraborty <rohit.chakraborty@oracle.com>
2019-12-11 12:20:14 +0530

    Fetch input json, can be shown in developer console

--- main_script.js
+++ main_script.js
@@ -1,5 +1,16 @@
 document.getElementById("content").innerHTML="Hello!";
 
+var inputJson = "";
 var request = new XMLHttpRequest();
-request.open("GET", "input.json");
+//request.open("GET", "input.json");
+request.open("GET","https://rohchakr.github.io/SpeedTyping/input.json");
+request.onreadystatechange = function() {
+  if(request.readyState===4 && request.status===200) {
+    inputJson = JSON.parse(request.responseText);
+    // we can initially have some progress image and stop that here
+  }
+}
 request.send();
+
+// need to do exception handling for the above xhr
+
